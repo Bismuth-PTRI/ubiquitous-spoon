248dbde1eb78cd36372d69d6910f51aa1e9670e9
Robert Luo <robertxluo@gmail.com>
2020-06-16 21:15:59 -0500

    Implement addFavorite controller

--- server/controllers/favoritesController.js
+++ server/controllers/favoritesController.js
@@ -6,8 +6,21 @@ favoritesController.getFavorites = (req, res, next) => {
   next();
 };
 
-favoritesController.addFavorite = (req, res, next) => {
-  next();
+favoritesController.addFavorite = async (req, res, next) => {
+  const { username, recipeId } = req.body;
+  const userIdQuery = `SELECT id FROM ubiquitous_spoon.users WHERE username='${username}'`;
+  try {
+    const { rows } = await pool.query(userIdQuery);
+    const userId = rows[0].id;
+
+    const addFavoriteQuery = `INSERT INTO ubiquitous_spoon.favorites (user_id, recipe_id)
+    VALUES ($1, $2)`;
+    const addFavoriteValues = [userId, recipeId];
+    await pool.query(addFavoriteQuery, addFavoriteValues);
+    next();
+  } catch (err) {
+    next({ log: `addFavorite controller error: ${err.message}`, message: { err: 'An error with adding favorite occurered' } });
+  }
 };
 
 favoritesController.deleteFavorite = (req, res, next) => {
