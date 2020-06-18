7cefa20fb5117dbe5acda55644515b91cafc0345
Ma-Christopher <christopher.john.ma@gmail.com>
2020-06-17 18:45:04 -0700

    This adds conditional rendering to the navbar based on whether or not a user is logged in

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Layout, Menu, Breadcrumb } from 'antd';
+import { connect } from 'react-redux';
 
-const Nav = () => {
+const mapStateToProps = (state) => ({
+  user: state.user.username,
+});
+
+const mapDispatchToProps = {};
+
+const Nav = (props) => {
   return (
     <>
       <div className="logo" />
@@ -11,22 +18,31 @@ const Nav = () => {
           <Menu.Item key="1">
             <Link to="/">Home</Link>
           </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/profile">My Profile</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="/favorites">Saved Recipes</Link>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="/login">Login</Link>
-          </Menu.Item>
-          <Menu.Item key="5">
-            <Link to="/signup">Signup</Link>
-          </Menu.Item>
+          {props.user && (
+            <Menu.Item key="2">
+              <Link to="/profile">My Profile</Link>
+            </Menu.Item>
+          )}
+          {props.user && (
+            <Menu.Item key="3">
+              <Link to="/favorites">Saved Recipes</Link>
+            </Menu.Item>
+          )}
+          {!props.user && (
+            <Menu.Item key="4">
+              <Link to="/login">Login</Link>
+            </Menu.Item>
+          )}
+          {!props.user && (
+            <Menu.Item key="5">
+              <Link to="/signup">Signup</Link>
+            </Menu.Item>
+          )}
+          {props.user && <Menu.Item key="6">Logout Placeholder</Menu.Item>}
         </Menu>
       </div>
     </>
   );
 };
 
-export default Nav;
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
