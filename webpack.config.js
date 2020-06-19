a8d82ba7f6a6d9d548b455274efc6ce58cf23fc8
Matt Digel <DIGEL.MATT@GMAIL.COM>
2020-06-18 19:38:50 -0700

    Homepage done expect for Favoriting

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,35 @@ module.exports = {
       {
         test: /.(css|scss|sass)$/,
         exclude: /node_modules(?!\/antd)/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'less-loader'],
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'less-loader', // compiles Less to CSS
+            options: {
+              lessOptions: {
+                // If you are using less-loader@5 please spread the lessOptions to options directly
+                modifyVars: {
+                  'primary-color': '#A294F6',
+                  'link-color': '#1890ff',
+                  'success-color': '#52c41a',
+                  'warning-color': '#faad14',
+                  'error-color': '#f5222d',
+                  'font-size-base': '14px',
+                  'heading-color': 'rgba(0, 0, 0, 0.85)',
+                  'text-color': 'rgba(0, 0, 0, 0.65)',
+                  'text-color-secondary': 'rgba(0, 0, 0, 0.45)',
+                  'disabled-color': 'rgba(0, 0, 0, 0.25)',
+                  'border-radius-base': '4px',
+                  'border-color-base': '#d9d9d9',
+                  'box-shadow-base': '0 2px 8px rgba(0, 0, 0, 0.15)',
+                },
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.less$/,
@@ -46,17 +74,27 @@ module.exports = {
           },
           {
             loader: 'less-loader', // compiles Less to CSS
-            // options: {
-            //   lessOptions: {
-            //     // If you are using less-loader@5 please spread the lessOptions to options directly
-            //     modifyVars: {
-            //       'primary-color': '#1DA57A',
-            //       'link-color': '#1DA57A',
-            //       'border-radius-base': '2px',
-            //     },
-            //     javascriptEnabled: true,
-            //   },
-            // },
+            options: {
+              lessOptions: {
+                // If you are using less-loader@5 please spread the lessOptions to options directly
+                modifyVars: {
+                  'primary-color': '#A294F6',
+                  'link-color': '#1890ff',
+                  'success-color': '#52c41a',
+                  'warning-color': '#faad14',
+                  'error-color': '#f5222d',
+                  'font-size-base': '14px',
+                  'heading-color': 'rgba(0, 0, 0, 0.85)',
+                  'text-color': 'rgba(0, 0, 0, 0.65)',
+                  'text-color-secondary': 'rgba(0, 0, 0, 0.45)',
+                  'disabled-color': 'rgba(0, 0, 0, 0.25)',
+                  'border-radius-base': '4px',
+                  'border-color-base': '#d9d9d9',
+                  'box-shadow-base': '0 2px 8px rgba(0, 0, 0, 0.15)',
+                },
+                javascriptEnabled: true,
+              },
+            },
           },
         ],
       },
