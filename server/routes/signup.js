ca2943da16a52798624f586c383a55b6ce4fd6b0
Matt Digel <DIGEL.MATT@GMAIL.COM>
2020-06-30 20:11:05 -0700

    added createJWT

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,9 +1,19 @@
 const express = require('express');
+
 const router = express.Router();
 const usersController = require('../controllers/usersController');
+const jwtController = require('../controllers/jwtController');
 
-router.post('/', usersController.checkUsername, usersController.checkPassword, usersController.addUser, usersController.createSession, (req, res) => {
-  res.status(200).json({ success: true, username: res.locals.username });
-});
+router.post(
+  '/',
+  usersController.checkUsername,
+  usersController.checkPassword,
+  usersController.addUser,
+  usersController.createSession,
+  jwtController.createJWT,
+  (req, res) => {
+    res.status(200).json({ success: true, username: res.locals.username, token: res.locals.token });
+  }
+);
 
 module.exports = router;
