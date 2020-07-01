eea03755a03b2f984e1fbd9584ac6082d5b8282f
Matt Digel <DIGEL.MATT@GMAIL.COM>
2020-07-01 11:35:07 -0700

    remove clearSession controller

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,22 +4,16 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 const jwtController = require('../controllers/jwtController');
 
-router.post(
-  '/',
-  usersController.checkLogin,
-  usersController.createSession,
-  jwtController.createJWT,
-  (req, res) => {
-    res.status(200).json({
-      success: true,
-      username: res.locals.username,
-      vegan: res.locals.vegan,
-      vegetarian: res.locals.vegetarian,
-      glutenFree: res.locals.glutenFree,
-      token: res.locals.token,
-      token_expiry: res.locals.token_expiry,
-    });
-  }
-);
+router.post('/', usersController.checkLogin, jwtController.createJWT, (req, res) => {
+  res.status(200).json({
+    success: true,
+    username: res.locals.username,
+    vegan: res.locals.vegan,
+    vegetarian: res.locals.vegetarian,
+    glutenFree: res.locals.glutenFree,
+    token: res.locals.token,
+    token_expiry: res.locals.token_expiry,
+  });
+});
 
 module.exports = router;
