5d16baecd784bdb1be34677b7819388ad5fce553
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-03 03:26:32 +0100

    refined changes for db model changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const fs = require('fs');
 const lessToJs = require('less-vars-to-js');
-const themeVariables = lessToJs(fs.readFileSync(path.join(__dirname, './src/style/theme.less'), 'utf8'));
+const themeVariables = lessToJs(
+  fs.readFileSync(path.join(__dirname, './src/style/theme.less'), 'utf8')
+);
 
 module.exports = {
   entry: {
