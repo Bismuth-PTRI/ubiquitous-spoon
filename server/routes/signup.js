087f59bb0e98396f1c36caea2bdb743c0bc40c9c
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-06 22:58:10 +0100

    completed new preference controller middleware into signup pipeline

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -3,12 +3,14 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 const jwtController = require('../controllers/jwtController');
+const preferenceController = require('../controllers/preferenceController');
 
 router.post(
   '/',
   usersController.checkUsername,
   usersController.checkPassword,
   usersController.addUser,
+  preferenceController.addUserPreferences,
   jwtController.createJWT,
   (req, res) => {
     res.status(200).json({
