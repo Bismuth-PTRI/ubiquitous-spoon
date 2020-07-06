207f343cd5d8322f32e17412f192982b6b565179
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-06 22:23:57 +0100

    added redux state props to functional component to resolve issue with state vairables (diets)  not updating when loaded from BE

--- src/components/DietPrefs.jsx
+++ src/components/DietPrefs.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Form, Checkbox } from 'antd';
 
 // https://spoonacular.com/food-api/docs#Diets
@@ -9,15 +9,21 @@ import { Form, Checkbox } from 'antd';
 // Whole30
 // Ketogenic
 
-const DietPrefs = ({ prefsList, pushPrefs, ...props }) => {
-  const [checkOptions] = useState(Object.values(props).map((m) => m.value));
+const mapStateToProps = (state) => {
+  return {
+    items: state.food.diets,
+  };
+};
+
+const DietPrefs = (props) => {
+  const [checkOptions] = useState(Object.values(props.items).map((m) => m.value));
   return (
     <div>
       <Form.Item name="checkbox-group" label="Food Preferences">
-        <Checkbox.Group options={checkOptions} onChange={(e) => pushPrefs(e)} />
+        <Checkbox.Group options={checkOptions} onChange={(e) => props.pushPrefs(e)} />
       </Form.Item>
     </div>
   );
 };
 
-export default DietPrefs;
+export default connect(mapStateToProps)(DietPrefs);
