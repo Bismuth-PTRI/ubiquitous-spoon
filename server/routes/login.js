6e6d3366f90a239b48f50fcd3738fb527466e3f2
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-07 01:26:59 +0100

    mounted new preference middleware to the login route

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,17 +3,25 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 const jwtController = require('../controllers/jwtController');
+const preferenceController = require('../controllers/preferenceController');
 
-router.post('/', usersController.checkLogin, jwtController.createJWT, (req, res) => {
-  res.status(200).json({
-    success: true,
-    username: res.locals.username,
-    vegan: res.locals.vegan,
-    vegetarian: res.locals.vegetarian,
-    glutenFree: res.locals.glutenFree,
-    token: res.locals.token,
-    token_expiry: res.locals.token_expiry,
-  });
-});
+router.post(
+  '/',
+  usersController.checkLogin,
+  preferenceController.fetchUserPreferences,
+  jwtController.createJWT,
+  (req, res) => {
+    res.status(200).json({
+      success: true,
+      username: res.locals.username,
+      // vegan: res.locals.vegan,
+      // vegetarian: res.locals.vegetarian,
+      // glutenFree: res.locals.glutenFree,
+      preferences: res.locals.userpreferences,
+      token: res.locals.token,
+      token_expiry: res.locals.token_expiry,
+    });
+  }
+);
 
 module.exports = router;
