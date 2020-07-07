80df79158d6bceda435b00a952a838dae997e5c8
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-07 01:26:15 +0100

    fixed implementation issues with preference controller

--- server/controllers/utility.js
+++ server/controllers/utility.js
@@ -4,7 +4,6 @@ const userId = async (username) => {
   try {
     const queryTxt = `SELECT id FROM users WHERE username = '${username}'`;
     const { rows } = await pool.query(queryTxt);
-    console.log('User Id Search :: ', rows);
     return { status: true, value: rows[0].id };
   } catch (err) {
     return { status: false, value: err };
