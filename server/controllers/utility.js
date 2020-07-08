54fbd34047ad01c46878a1559c2a0e18987245b7
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-08 03:52:19 +0100

    added handler utility for removal of user's previous preferences and insertion of user preferences to be called by update and add user preference methods on the preference controller

--- server/controllers/utility.js
+++ server/controllers/utility.js
@@ -11,4 +11,34 @@ const userId = async (username) => {
   }
 };
 
-module.exports = { userId };
+// utility to insert all food preferences on the req object. Should be invoked from
+// addUserPreferences & updateUserPreferences
+const insertUserPreferences = async (reqObj, user_id) => {
+  let prefString = '';
+
+  reqObj.diet.map((diet) => (prefString += `(${user_id.value}, ${diet}),`));
+  reqObj.intolerance.map((intolerance) => (prefString += `(${user_id.value}, ${intolerance}),`));
+  prefString = prefString.slice(0, -1);
+
+  const text = `INSERT INTO userpreference (user_id, preference_id)
+  VALUES ${prefString};`;
+  try {
+    await pool.query(text);
+    return { status: true, value: 'done' };
+  } catch (err) {
+    return { status: false, value: err };
+  }
+};
+
+// utility to remove previously selected user preferences befoer inserting another in update user preferences
+const clearUserPreferences = async (user_id) => {
+  try {
+    const queryTxt = `DELETE FROM userpreference WHERE user_id = ${user_id}`;
+    await pool.query(queryTxt);
+    return { status: true, value: 'done' };
+  } catch (err) {
+    return { status: false, value: err };
+  }
+};
+
+module.exports = { userId, insertUserPreferences, clearUserPreferences };
